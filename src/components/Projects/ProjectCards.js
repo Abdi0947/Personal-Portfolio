d6b3410f0e4a2748e2201522b03be95b62951759
abdi <debelaabdi3@gmail.com>
2024-07-26 23:21:17 +0300

    projects

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -5,9 +5,10 @@ import { CgWebsite } from "react-icons/cg";
 import { BsGithub } from "react-icons/bs";
 
 function ProjectCards(props) {
+  
   return (
     <Card className="project-card-view">
-      <Card.Img variant="top" src={props.imgPath} alt="card-img" />
+      <Card.Img variant="top" src={props.imgPath} alt="card-img"  onError={(e) => (console.log(e))}/>
       <Card.Body>
         <Card.Title>{props.title}</Card.Title>
         <Card.Text style={{ textAlign: "justify" }}>
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -19,16 +19,37 @@ function Projects() {
           <Col md={4} className="project-card">
             <ProjectCard
               isBlog={false}
-              title="clinic website" // Change this to your project title
+              imgPath="Projects/clinic.png"
+              title="clinic website" 
               description="clinic website offering easy appointment booking, access to medical records, and expert healthcare services."
               ghLink="https://github.com/Abdi0947/Clinic-website" 
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              isBlog={false}
+              imgPath="Projects/gpt-3"
+              title="GPT-3" 
+              description="the design layout of gpt-3,it is responsive and attractive."
+              ghLink="https://github.com/Abdi0947/GPT-3" 
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
               isBlog={false}
-              title="Jedidiah-choir" // Change this to your project title
+              imgPath="Projects/chatApp.jpg"
+              title="Chat Application" 
+              description="A modern, responsive chat application built with React, enabling seamless real-time messaging, file sharing, and user management."
+              ghLink="https://github.com/Abdi0947/chat_Application" 
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              isBlog={false}
+              imgPath="Projects/jedidiah.jpg'"
+              title="Jedidiah-choir" 
               description="Developed a choir website for performance showcases, event updates, and member engagement, enhancing online presence and audience interaction."
               ghLink="https://github.com/Abdi0947/JEDIDIAH-CHOIR" 
             />
