e22acc77f481614293fed2c95098dcb0eb7fe669
abdi <debelaabdi3@gmail.com>
2024-07-30 15:56:32 +0300

    modification

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -94,7 +94,7 @@ function NavBar() {
 
             <Nav.Item>
               <Nav.Link
-                href="https://soumyajitblogs.vercel.app/"
+                href="https://abdiblogs.vercel.app/"
                 target="_blank"
                 rel="noreferrer"
               >
